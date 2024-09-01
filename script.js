65d3f356078f5032e1d664eeb836bafb09d18022
yassine elmiri <yassireelhima@gmail.com>
2024-09-01 16:12:36 +0100

    Update : checkWin() : Cette fonction vérifie s'il y a une série de 5 symboles consécutifs dans les directions horizontale, verticale ou diagonale. Si une série est trouvée, elle met fin au jeu

--- script.js
+++ script.js
@@ -61,44 +61,20 @@ function handleBoxClick(event) {
 
 
 function changeTurn() {
-    if (turn === "X") {
-        turn = "O";
-        document.querySelector(".bg").style.left = "85px";
-    } else {
-        turn = "X";
-        document.querySelector(".bg").style.left = "0";
-    }
+    turn = turn === "X" ? "O" : "X";
+    document.querySelector(".bg").style.left = turn === "X" ? "0" : "85px";
+    saveGameState();
 }
 
 function checkWin() {
-    let winConditions = [
-        [0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14],
-        [15, 16, 17, 18, 19], [20, 21, 22, 23, 24],
-        [0, 5, 10, 15, 20], [1, 6, 11, 16, 21], [2, 7, 12, 17, 22],
-        [3, 8, 13, 18, 23], [4, 9, 14, 19, 24],
-        [0, 6, 12, 18, 24], [4, 8, 12, 16, 20]
-    ];
+    const winLength = 5;
 
-    for (let i = 0; i < winConditions.length; i++) {
-        let [a, b, c, d, e] = winConditions[i];
-        if (
-            boxes[a].innerHTML &&
-            boxes[a].innerHTML === boxes[b].innerHTML &&
-            boxes[a].innerHTML === boxes[c].innerHTML &&
-            boxes[a].innerHTML === boxes[d].innerHTML &&
-            boxes[a].innerHTML === boxes[e].innerHTML
-        ) {
-            isGameOver = true;
-            document.querySelector("#results").innerHTML = turn + " wins";
-            document.querySelector("#play-again").style.display = "inline";
-
-            [a, b, c, d, e].forEach(index => {
-                boxes[index].style.backgroundColor = "#08D9D6";
-                boxes[index].style.color = "#000";
-            });
-        }
+    if (checkDirection(winLength, checkHorizontal) || checkDirection(winLength, checkVertical) ||
+        checkDirection(winLength, checkDiagonalRight) || checkDirection(winLength, checkDiagonalLeft)) {
+        markWin();
     }
 }
+
 
 function checkDraw() {
     if (!isGameOver) {
